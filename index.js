44e34c8212853735235bc2bbe922c6a82bc631de
rhetthancock <hancockrhett@gmail.com>
2020-07-03 22:16:41 -0600

    Setup Express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
